66e4c3f0b03e97d548ca5ce4c41c226ff03fdc9b
Szymon Wygnański <sw@7willows.com>
2023-01-31 10:31:57 +0200

    fix: switched to using ts-pattern from esm.sh instead of npmjs.org

--- deps.ts
+++ deps.ts
@@ -1,7 +1,7 @@
 export { z } from "https://deno.land/x/zod@v3.20.2/mod.ts";
 export * as _ from "https://deno.land/x/lodash_es@v0.0.2/mod.ts";
 export { Reflect } from "https://deno.land/x/deno_reflect@v0.2.1/mod.ts";
-export { match, P } from "npm:ts-pattern@4.1.3";
+export { match, P } from "https://esm.sh/ts-pattern@4.1.3";
 export { Hono } from "https://deno.land/x/hono@v3.0.0-rc.8/mod.ts";
 export type { Context } from "https://deno.land/x/hono@v3.0.0-rc.8/mod.ts";
 export type { StatusCode } from "https://deno.land/x/hono@v3.0.0-rc.8/utils/http-status.ts";
