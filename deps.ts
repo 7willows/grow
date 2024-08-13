bcd391da1d5cabdf2eae6c7f1ea398b656235e54
Szymon Wygnański <sw@7willows.com>
2024-08-13 08:17:26 +0200

    chore: update hono

--- deps.ts
+++ deps.ts
@@ -1,8 +1,8 @@
 export { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
 export * as _ from "https://deno.land/x/lodash_es@v0.0.2/mod.ts";
 export { match, P } from "https://esm.sh/ts-pattern@4.2.2";
-export { Hono } from "jsr:@hono/hono@4.5.1";
-export type { Context } from "jsr:@hono/hono@4.5.1";
+export { Hono } from "jsr:@hono/hono@4.5.5";
+export type { Context } from "jsr:@hono/hono@4.5.5";
 export * as path from "https://deno.land/std@0.198.0/path/mod.ts";
 import * as uuid from "https://deno.land/std@0.198.0/uuid/mod.ts";
 export { existsSync } from "https://deno.land/std@0.198.0/fs/mod.ts";
